f4c6c0f20e494189f8be059ba7dc10a3d888440d
runnedrunner <auszhou@nuevaschool.org>
2016-10-28 08:55:50 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ var key1 = {
   "nestedKey2": "bye!"
 }
 var key2 = {
-  "nestedkey1": {"nestedkey2": {"nestedkKey5": {"notasNestedKey": "I'm not as nested!"}, {"nestedkey3": {"nestedKey4": {"reallyNestedKey:"}}}}
+  "nestedkey1": {"nestedkey2": {"nestedkKey5": {"notasNestedKey": "I'm not as nested!"}, "nestedkey3": {"nestedKey4": {"reallyNestedKey:"I'm deeply nested!"}}}}}
 }
 storeValue("key2", key2);
